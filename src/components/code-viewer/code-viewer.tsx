93f2fc412675be5f5a049bc906c3e94ca95487d8
dzulkiram hilmi <bukutulis0110@gmail.com>
2024-10-26 22:34:40 +0700

    feat: add CodeViewer component with Prism-react-renderer
    - Created CodeViewer component using prism-react-renderer for syntax highlighting.
    - Applied custom synthwave84 theme with transparent background for a modern look.
    - Implemented flexible language support and line numbering.

--- src/components/code-viewer/code-viewer.tsx
+++ src/components/code-viewer/code-viewer.tsx
@@ -1,117 +1,189 @@
+//code : https://github.com/Kinfe123/farm-ui/blob/main/apps/www/components/ui/Preview/index.tsx
+
 import React, { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Highlight, themes } from "prism-react-renderer";
 import { ReloadWrapper } from "../reload-wrapper";
+import { motion } from "framer-motion";
+// import synthwave84 from "./synthwave84";
+// import vsDark from "./vsDark";
+
+interface HighlightProps {
+  selectedFramework: string | null;
+}
+
+type PrismTheme = typeof themes.oneLight;
+
+// Create modified theme
+const customTheme: PrismTheme = {
+  ...themes.oneLight,
+  plain: {
+    ...themes.oneLight.plain,
+    backgroundColor: "transparent" as const,
+  },
+};
 
 const frameworkTabs = [
   {
-    name: "React.js",
+    name: "main.jsx",
     value: "react",
     icon: "⚛️",
   },
   {
-    name: "HTML",
+    name: "navbar.jsx",
     value: "html",
     icon: "🌐",
   },
   {
-    name: "Vue.js",
+    name: "footer.jsx",
     value: "vue",
     icon: "💚",
   },
   {
-    name: "Svelte.js",
+    name: "data.js",
     value: "svelte",
     icon: "🔥",
   },
 ];
 
-const sampleCode = `import {
-  CardTitle,
-  CardDescription,
-  CardHeader,
-  CardContent,
-  CardFooter,
-  Card,
-} from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { ChevronRight, Loader2 } from "lucide-react";
-import { useState, useTransition } from "react";
-import { useToast } from "@/components/ui/use-toast";
-// raplace it with your actions.
-import { saySomething } from "actions/emailSubRelated";
-import {Button} from "@/components/ui/button";
-type FormType = {
-  firstName: string;
-  lastName?: string;
-  email: string;
-  message: string;
-};`;
+const sampleCode = `import { AnimatedText } from "@/components/animated-text";
+import { CopyText } from "@/components/copy-text";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@components/ui/tooltip";
 
-const CodeViewer = () => {
+export default function Home() {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center relative overflow-hidden">
+      <div className="absolute -top-[118px] inset-0 bg-[linear-gradient(to_right,#222_1px,transparent_1px),linear-gradient(to_bottom,#222_1px,transparent_1px)] bg-[size:4.5rem_2rem] -z-10 [transform:perspective(1000px)_rotateX(-63deg)] h-[80%] pointer-events-none" />
+      <div className="absolute inset-0 bg-gradient-to-t from-background to-transparent pointer-events-none -z-10" />
+
+      <h1 className="font-departure text-[40px] md:text-[84px] relative z-10 text-center h-[120px] md:h-auto leading-tight">
+        <AnimatedText text="Production ready code" />
+      </h1>
+
+      <p className="relative z-10 text-center max-w-[80%] mt-0 md:mt-4">
+        An open-source starter kit based on{" "}
+        <a href="#" className="underline">
+          shadcn
+        </a>
+        .
+      </p>
+
+      <div className="mt-10 mb-8">
+        <CopyText value="bunx degit shadcn" />
+      </div>
+
+      <TooltipProvider delayDuration={0}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <a
+              href="#"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={16}
+                  height={16}
+                  fill="none"
+                >
+                  <g clipPath="url(#a)">
+                    <path
+                      fill="#F60"
+                      d="M0 0v16h16V0H0Zm8.7 9.225v3.925H7.275V9.225L3.775 2.3h1.65L8 7.525 10.65 2.3h1.55L8.7 9.225Z"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="a">
+                      <path fill="#fff" d="M0 0h16v16H0z" />
+                    </clipPath>
+                  </defs>
+                </svg>
+                <span className="text-sm">Live on Hacker News</span>
+              </div>
+            </a>
+          </TooltipTrigger>
+          <TooltipContent side="bottom" sideOffset={15} className="text-xs">
+            Shadcn – An open-source starter kit for your next project
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <div className="absolute -bottom-[280px] inset-0 bg-[linear-gradient(to_right,#222_1px,transparent_1px),linear-gradient(to_bottom,#222_1px,transparent_1px)] bg-[size:4.5rem_2rem] -z-10 [transform:perspective(560px)_rotateX(63deg)] pointer-events-none" />
+      <div className="absolute w-full bottom-[100px] h-1/2  bg-gradient-to-b from-background to-transparent pointer-events-none -z-10" />
+    </div>
+  );
+}`;
+
+const CodeViewer: React.FC<HighlightProps> = () => {
   const [selectedView, setSelectedView] = useState("code");
   const [selectedFramework, setSelectedFramework] = useState("react");
 
-  const getFrameworkContent = (framework) => {
+  const getFrameworkContent = (framework: string) => {
     if (framework === "react") {
       return sampleCode;
     }
-    return null;
+    return "";
   };
 
   return (
-    <ReloadWrapper>
-      <Tabs value={selectedView} onValueChange={setSelectedView} className="w-full relative border-none">
-        <TabsList className=" w-fit">
-          <TabsTrigger value="preview">Preview</TabsTrigger>
-          <TabsTrigger value="code">Code</TabsTrigger>
-        </TabsList>
+    <div className="w-full">
+      <ReloadWrapper className="px-4 py-8 ">
+        <Tabs value={selectedView} onValueChange={setSelectedView} className="w-full relative overflow-y-scroll h-[36rem] font-mono border-none">
+          <TabsList className=" w-fit">
+            <TabsTrigger value="preview">Preview</TabsTrigger>
+            <TabsTrigger value="code">Code</TabsTrigger>
+          </TabsList>
 
-        <TabsContent value="preview" className="p-4">
-          <div className="text-center p-8 text-gray-500">On Development</div>
-        </TabsContent>
+          <TabsContent value="preview" className="p-4">
+            <div className="text-center p-8 text-gray-500">On Development</div>
+          </TabsContent>
 
-        <TabsContent value="code">
-          <Tabs value={selectedFramework} onValueChange={setSelectedFramework} className="w-full">
-            <TabsList className="grid w-full grid-cols-4 place-self-center items-center justify-between">
-              {/* <TabsList className="flex items-center max-w-[76rem] px-4 py-2 border-b border-b-zinc-800 overflow-auto p-2 bg-zinc-800/5"> */}
-              {frameworkTabs.map((tab) => (
-                <TabsTrigger key={tab.value} value={tab.value} className="flex items-start justify-start gap-2 w-fit">
-                  <span>{tab.icon}</span>
-                  {tab.name}
-                </TabsTrigger>
-              ))}
-            </TabsList>
-
-            <div className="p-4 bg-zinc-900 rounded-b-lg">
-              {getFrameworkContent(selectedFramework) ? (
-                <Highlight theme={themes.nightOwl} code={getFrameworkContent(selectedFramework)} language="jsx">
-                  {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                    <pre className={`${className} overflow-auto`} style={style}>
-                      {tokens.map((line, i) => (
-                        <div key={i} {...getLineProps({ line })}>
-                          <span className="text-gray-500 mr-4">{i + 1}</span>
-                          {line.map((token, key) => (
-                            <span key={key} {...getTokenProps({ token })} />
+          <TabsContent value="code">
+            <Tabs value={selectedFramework} onValueChange={setSelectedFramework} className="w-full">
+              <TabsList className="grid  grid-cols-4 place-self-start items-center justify-between mb-2">
+                {/* <TabsList className="flex items-center max-w-[76rem] px-4 py-2 border-b border-b-zinc-800 overflow-auto p-2 bg-zinc-800/5"> */}
+                {frameworkTabs.map((tab) => (
+                  <TabsTrigger key={tab.value} value={tab.value} className="flex items-start justify-start gap-2 w-fit">
+                    <span>{tab.icon}</span>
+                    {tab.name}
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+                <motion.div className=" h-[640px] p-4 bg-zinc-900 overflow-y-auto w-full  rounded-b-lg">
+                  {getFrameworkContent(selectedFramework) ? (
+                    <Highlight theme={customTheme} code={getFrameworkContent(selectedFramework)} language="jsx">
+                      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+                        <pre className={`${className} overflow-auto`} style={style}>
+                          {tokens.map((line, i) => (
+                            <div key={i} {...getLineProps({ line })}>
+                              <span className="text-gray-500 mr-4">{i + 1}</span>
+                              {line.map((token, key) => (
+                                <span key={key} {...getTokenProps({ token })} />
+                              ))}
+                            </div>
                           ))}
-                        </div>
-                      ))}
-                    </pre>
+                        </pre>
+                      )}
+                    </Highlight>
+                  ) : (
+                    <div className="flex flex-col items-center justify-center py-12 text-gray-400">
+                      {/* <Github className="w-8 h-8 mb-4" /> */}
+                      <p className="text-sm">Be the first to contribute!</p>
+                    </div>
                   )}
-                </Highlight>
-              ) : (
-                <div className="flex flex-col items-center justify-center py-12 text-gray-400">
-                  {/* <Github className="w-8 h-8 mb-4" /> */}
-                  <p className="text-sm">Be the first to contribute!</p>
-                </div>
-              )}
-            </div>
-          </Tabs>
-        </TabsContent>
-      </Tabs>
-    </ReloadWrapper>
+                </motion.div>
+              
+            </Tabs>
+          </TabsContent>
+        </Tabs>
+      </ReloadWrapper>
+    </div>
   );
 };
 
--- src/components/code-viewer/lazy-motion-wrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { LazyMotion, domAnimation } from "framer-motion";
-import { ReactNode } from "react";
-
-const LazyMotionWrapper = ({ children }: { children: ReactNode }) => <LazyMotion features={domAnimation}>{children}</LazyMotion>;
-
-export default LazyMotionWrapper;
